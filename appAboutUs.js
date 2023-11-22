3ba3a271ddfbcfe17bd7c6d7cc77623b32630bf6
Viacheslav Liabin <R9514633542@gmail.com>
2023-11-22 19:43:13 +0500

    revisions from 11/21/23

--- appAboutUs.js
+++ appAboutUs.js
@@ -12,7 +12,7 @@ let dotIndex = 0;
 let linkIndex = dotIndex;
 
 const nextSlide = () => {
-  if (position < (dots.length - 1) * 398) {
+  if (position < (11 - 1) * 398) {
     position += 398;
   } else {
     position = 0;
@@ -39,7 +39,7 @@ const prevSlide = () => {
   if (position > 0) {
     position -= 398;
   } else {
-    position = (dots.length - 1) * 398;
+    position = (11 - 1) * 398;
   }
   sliderLine.style.left = -position + "px";
   thisSlide(dotIndex);
@@ -105,158 +105,100 @@ prevButton.addEventListener("click", prevSlideText);
 //   });
 // });
 
-/*Закрытие формы по клику на фон*/
-document.addEventListener('click', (e) => {
-  const click1 = e.composedPath().includes(applicationForm);
-  const click3 = e.composedPath().includes(showButtonHeader);
-  const click4 = e.composedPath().includes(falseButton);
-  const click5 = e.composedPath().includes(popUp);
+/*Кастомный селект*/
 
-  if ( !click1 && !click3 && !click4 && !click5) {
-    header.style.backgroundColor =  '#FFF';
-    body.style.backgroundColor =  '#FFF';
-    applicationForm.style.display = 'none';
-    body.style.overflow = 'auto';
-    dropDownCloseMain.style.display = "none";
-    dropDownOpenMain.style.display = "block";
-    dropDownListMain.style.display = "none";
-    applicationButton.style.display = "block";
-    applicationCheckbox.style.display = "block";
-    headerCity.innerHTML = popUpCity.value;
-    popUp.style.display = "none";
-    yourCity.innerHTML = popUpCity.value;
+const element = document.querySelector(".main-select");
+const choices = new Choices(element, {
+  searchEnabled: false,
+  itemSelectText: "",
+  noResultsText: "По Вашему запросу ничего не найдено",
+});
 
-    if (headerCity.innerHTML === '') {
-      headerCity.innerHTML = 'Иркутск';
-      yourCity.innerHTML = 'Иркутск';
-    }
-  }
-})
-
-const popUp = document.querySelector(".popUp");
-
-/*Главная форма "Оставить заявку" на главной странице*/
-const body = document.querySelector("body");
+/*Show-hidden mainForm*/
 const showButtonHeader = document.querySelector(".header-nav__button");
-const applicationForm = document.querySelector(".application-form")
 const closeButton = document.querySelector(".application-close");
+const applicationForm = document.querySelector(".application-form");
+const body = document.querySelector("body");
 const header = document.querySelector("#header-section");
 
-showButtonHeader.addEventListener("click", showForm)
+showButtonHeader.addEventListener("click", showForm);
 
 function showForm() {
-  body.style.overflow =  'hidden'; // заперт фоновой прокрутки
-  header.style.backgroundColor =  'rgb(245,245,245)';
-  body.style.backgroundColor =  'rgba(0, 0, 0, 0.05)';
+  header.style.position = "relative";
+  header.style.backgroundColor = "rgba(0, 0, 0, 0.01)";
+  body.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
   applicationForm.style.display = "flex";
 }
 
-closeButton.addEventListener("click", hiddenForm)
+closeButton.addEventListener("click", hiddenForm);
 
 function hiddenForm() {
-  body.style.overflow =  'auto';
-  header.style.backgroundColor =  '#FFF';
-  body.style.backgroundColor =  '#FFF';
+  header.style.position = "sticky";
+  header.style.backgroundColor = "#FFF";
+  body.style.backgroundColor = "#FFF";
   applicationForm.style.display = "none";
 }
 
-
-/*Показать/скрыть dropDown в форме заявки*/
-const dropDownOpenMain = document.querySelector(".dropDown-main-img-down");
-const dropDownCloseMain = document.querySelector(".dropDown-main-img-up");
-const dropDownListMain = document.querySelector(".dropDown-main-list");
-const dropDownSelectCityMain = document.querySelector(".dropDown-main-selectCity");
-const applicationButton = document.querySelector(".application-button");
-const applicationCheckbox = document.querySelector(".application-checkbox");
-
-
-
-
-dropDownOpenMain.addEventListener("click",openDropDown)
-
-function openDropDown() {
-  applicationButton.style.display = "none";
-  applicationCheckbox.style.display = "none";
-  dropDownOpenMain.style.display = "none";
-  dropDownCloseMain.style.display = "block";
-  dropDownListMain.style.display = "block";
-  dropDownSelectCityMain.style.border = "1px solid #FDB913";
-}
-
-dropDownCloseMain.addEventListener("click",closeDropDown)
-
-function closeDropDown() {
-  applicationButton.style.display = "block";
-  applicationCheckbox.style.display = "block";
-  dropDownCloseMain.style.display = "none";
-  dropDownOpenMain.style.display = "block";
-  dropDownListMain.style.display = "none";
-  dropDownSelectCityMain.style.border = "1px solid #CBCCCE";
-}
-
-/*Извлечение города в dropDown*/
-const dropDownCityMain = document.querySelector(".dropDown-main-city");
-dropDownListMain.addEventListener("click", (e) => {
-    let content = e.target.innerHTML;
-    dropDownCityMain.value = content;
-    dropDownCloseMain.style.display = "none";
-    dropDownOpenMain.style.display = "block";
-    dropDownListMain.style.display = "none";
-    applicationButton.style.display = "block";
-    applicationCheckbox.style.display = "block";
-  });
-
-
-/*Показать/скрыть форму для выбора города*/
+/*Show-hidden cityForm*/
 const showExpand = document.querySelector(".header-expandMore");
 const showCity = document.querySelector(".header-city");
 const actualCity = document.querySelector(".actual-city");
 const trueButton = document.querySelector(".actual-city__button--yellow");
 const falseButton = document.querySelector(".actual-city__button--white");
-const yourCity = document.querySelector(".your-city");
 
-
-showExpand.addEventListener("click",showSlectCity)
-showCity.addEventListener("click",showSlectCity)
+showExpand.addEventListener("click", showSlectCity);
+showCity.addEventListener("click", showSlectCity);
 
 function showSlectCity() {
-    actualCity.style.display = "flex";
+  actualCity.style.display = "flex";
 }
 
-trueButton.addEventListener("click",closeSlectCity)
-falseButton.addEventListener("click",closeSlectCity)
-falseButton.addEventListener("click",showPopUp)
+trueButton.addEventListener("click", closeSlectCity);
+falseButton.addEventListener("click", closeSlectCity);
+falseButton.addEventListener("click", showPopUp);
 
 function closeSlectCity() {
   actualCity.style.display = "none";
 }
 
-/*Показать/скрыть popUp*/
+/*Show-hidden popUp*/
+const popUp = document.querySelector(".popUp");
+
 function showPopUp() {
-  body.style.overflow =  'hidden';
-  body.style.backgroundColor = 'red';
   popUp.style.display = "block";
-  header.style.backgroundColor = 'rgb(245,245,245)';
-  body.style.backgroundColor =  'rgba(0, 0, 0, 0.05)';
+  header.style.position = "relative";
+  header.style.backgroundColor = "rgba(0, 0, 0, 0.01)";
+  body.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
 }
 
 const popUpClose = document.querySelector(".popUp-close");
 
-popUpClose.addEventListener("click",closePopUp)
+popUpClose.addEventListener("click", closePopUp);
 
 function closePopUp() {
-  body.style.overflow =  'auto';
   popUp.style.display = "none";
-  header.style.backgroundColor =  '#FFF';
-  body.style.backgroundColor =  '#FFF';
-  headerCity.innerHTML = popUpCity.value;
-  yourCity.innerHTML = popUpCity.value;
+  header.style.position = "sticky";
+  header.style.backgroundColor = "#FFF";
+  body.style.backgroundColor = "#FFF";
 }
 
 /*Извлечение города в popUp*/
-const headerCity = document.querySelector('.header-city')
-const popUpCity = document.querySelector('.popUp-city')
-const popUpList = document.querySelector('.popUp-list').addEventListener('click', e => {
-let content2 = e.target.innerHTML;
-popUpCity.value = content2;
-});
+const popUpCity = document.querySelector(".popUp-city");
+const popUpList = document
+  .querySelector(".popUp-list")
+  .addEventListener("click", (e) => {
+    let content = e.target.innerHTML;
+    popUpCity.value = content;
+  });
+
+/*Скрытие кнопки*/
+/*
+const activeVideoAboutUs = document.querySelector(".video-aboutUs");
+const btnPlay = document.querySelector(".btn-play");
+
+activeVideoAboutUs.addEventListener("click", toggleStateVideo)
+
+function toggleStateVideo() {
+  btnPlay.style.display = btnPlay.style.display === 'none' ? 'block' : 'none';
+}
+*/
